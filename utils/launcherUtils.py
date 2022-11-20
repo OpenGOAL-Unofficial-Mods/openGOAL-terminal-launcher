f10541d14641d50b96cb2b7b6176215340086ae5
ZedB0T <89345505+Zedb0T@users.noreply.github.com>
2022-11-20 16:46:32 -0500

    fix

--- utils/launcherUtils.py
+++ utils/launcherUtils.py
@@ -295,7 +295,7 @@ def launch(URL, MOD_NAME, LINK_TYPE):
         try_kill_process("gk.exe")
         try_kill_process("goalc.exe")
         print("Done update starting extractor This one can take a few moments! \n")
-        extractor_command_list = [InstallDir +"\extractor.exe", "-f", iso_path]
+        extractor_command_list = [os.path.join(InstallDir,"extractor.exe"), "-f", iso_path]
         print(extractor_command_list)
         
         subprocess.Popen(extractor_command_list)
